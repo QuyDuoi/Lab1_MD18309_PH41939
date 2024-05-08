976ecb5d6e6e33cbb0c295acd2627eb063871b92
QuyDuoi <quynsph41939@fpt.edu.vn>
2024-05-08 23:55:57 +0700

    Initial commit

---
+++ app/src/main/java/com/lab1_md18309_ph41939/Lab1_Bai2.kt
@@ -0,0 +1,14 @@
+package com.lab1_md18309_ph41939
+
+fun main() {
+    print("Nguyễn Sỹ Quý - PH41939\n")
+    print("=========================\n")
+    println("Quanh năm buôn bán ở mom sông")
+    println("Nuôi đủ năm con với một chồng")
+    println("\tlặn lội thân cờ khi quãng vắng")
+    println("\teo sèo mặt nước buổi đò đồng")
+    println("Một duyên hai nợ âu đành phận")
+    println("Năm nắng mười mưa há chẳng công")
+    println("\tCha mẹ thói đời \"ăn ở bạc\"")
+    println("\tCó chồng hờ hững cũng như không")
+}
---
+++ app/src/main/java/com/lab1_md18309_ph41939/Lab1_Bai3.kt
@@ -0,0 +1,16 @@
+package com.lab1_md18309_ph41939
+
+fun main() {
+    var a = 0.0
+    var b = 0.0
+    println("Nhập a:")
+    var s = readLine()
+    if (s != null) a = s.toDouble()
+    println("Nhập b:")
+    s = readLine()
+    if (s != null) b = s.toDouble()
+    println("Tổng: " + (a + b))
+    println("Hiệu: " + (a - b))
+    println("Tích: " + (a * b))
+    println("Thương: " + (a / b))
+}
